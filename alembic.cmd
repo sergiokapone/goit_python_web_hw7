5719a9cf8efb7330ab96275a1a252a598037baf2
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-06-02 18:25:06 +0300

    add alembic

---
+++ alembic.cmd
@@ -0,0 +1,50 @@
+@echo off
+chcp 65001 > nul
+
+set ALEMBIC_PATH=.venv\Scripts
+set ALEMBIC_CONFIG=alembic.ini
+
+:menu
+REM Меню выбора действия
+cls
+echo 1. Сгенерировать миграцию
+echo 2. Применить все миграции
+echo 3. Откатить последнюю миграцию
+echo 4. Просмотреть текущее состояние миграций
+echo 5. Выйти
+
+set /p CHOICE=">>> "
+if %CHOICE%==1 goto generate
+if %CHOICE%==2 goto upgrade
+if %CHOICE%==3 goto downgrade
+if %CHOICE%==4 goto current_state
+if %CHOICE%==5 exit
+echo.
+
+goto menu
+
+REM Команда для створення порожньої міграції
+:generate
+set /p MIGRATION_NAME="Выберите имя миграции: "
+%ALEMBIC_PATH%\alembic.exe -c %ALEMBIC_CONFIG% revision --autogenerate -m "%MIGRATION_NAME%"
+pause
+goto menu
+
+REM Команда для застосування всіх міграцій
+:upgrade
+%ALEMBIC_PATH%\alembic.exe -c %ALEMBIC_CONFIG% upgrade head
+pause
+goto menu
+
+REM Команда для відкату останньої міграції
+:downgrade
+%ALEMBIC_PATH%\alembic.exe -c %ALEMBIC_CONFIG% downgrade -1
+pause
+goto menu
+
+REM Команда для перегляду поточного стану міграцій
+:current_state
+%ALEMBIC_PATH%\alembic.exe -c %ALEMBIC_CONFIG% current
+pause
+goto menu
+
--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy import MetaData
+
 
 from alembic import context
 
@@ -16,9 +18,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from database.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
