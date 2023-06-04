68135ba22f19eb9bc64cdc7d62cb6c951565d6d7
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-06-04 08:49:52 +0300

    possibility to save base on host

--- docker_run.cmd
+++ docker_run.cmd
@@ -1,14 +1,18 @@
 @echo off
-REM Проверяем наличие контейнера
+REM Перевіряємо наявність контейнера
 docker ps -a -f name=students | findstr students
 IF %ERRORLEVEL% EQU 0 (
-    REM Контейнер присутствует, удаляем его
     docker rm --force students
     echo Container "students" was deleted.
 ) ELSE (
-    REM Контейнер отсутствует
     echo Container "students" not found.
 )
 
-REM Запускаем новый контейнер
+SET "CURRENT_PATH=%~dp0"
+
+:: Для збереження бази на хостововій машиші
+::docker volume create students_data
+::docker run --name students -p 5432:5432 -e POSTGRES_PASSWORD=password -v %CURRENT_PATH%/db:/var/lib/postgresql/data -d postgres
+
+::Для збереження бвзи в контейнері
 docker run --name students -p 5432:5432 -e POSTGRES_PASSWORD=password -d postgres
